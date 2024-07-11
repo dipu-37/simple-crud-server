4bb648d16624f7828115c91d4088581dcfd31c49
dipu-37 <razibul1105nstu@gmail.com>
2024-07-12 02:57:41 +0700

    get post delete

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection URI
+const uri = "mongodb+srv://razibul1105nstu:lQWkdrA9BSe4Rb3H@cluster0.gjzedcz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    console.log("Connected to MongoDB!");
+
+    const userCollection = client.db('usersDB').collection('users');
+
+    app.get('/users',async (req, res)=>{
+      const cursor = userCollection.find()
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // POST endpoint to add a new user
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      console.log('New user:', user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // delete users from db 
+
+     app.delete('/users/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+
+        const user = await userCollection.deleteOne(query);
+        res.send(user);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Call the run function and handle errors
+run().catch(console.dir);
+
+// Default endpoint
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
